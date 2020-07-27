30c4a1bc46dd1bf375b484dd50b3577d64dbe90a
RJones <oskar_skilled_@live.com>
2020-07-27 00:44:30 -0500

    Pruebas unitarias para la clase Producto e IngresoDatos

--- project-git/src/main/java/ec/edu/epn/proyecto/git/IngresoDatos.java
+++ project-git/src/main/java/ec/edu/epn/proyecto/git/IngresoDatos.java
@@ -3,36 +3,64 @@ package ec.edu.epn.proyecto.git;
 import java.util.Scanner;
 
 public class IngresoDatos {
-
-	public static void main(String[] args) {
-		Producto prod = new Producto();
-		Scanner sc = new Scanner(System.in);
+	
+	Producto p = new Producto();
+	Scanner sc = new Scanner(System.in);
+		
+	public String nombrePro() {
+		String obo = "";
 		System.out.println("Por favor ingrese el nombre del producto: ");
-		prod.setNombre(sc.nextLine());
-		
-		System.out.println("Por favor ingrese el peso del producto: ");
-		prod.setPeso(sc.nextFloat());
-		
-		System.out.println("Por favor ingrese la cantidad de productos: ");
-		prod.setCantidad(sc.nextInt());
-		
-		System.out.println("Por favor ingrese el tipo de producto: ");
-		prod.setTipo(sc.next());
-		
-		System.out.println("Por favor ingrese el ancho del producto: ");
-		prod.setAncho(sc.nextFloat());
-		
-		System.out.println("Por favor ingrese el alto del producto: ");
-		prod.setAlto(sc.nextFloat());
-		
-		System.out.println("Por favor ingrese el largo del producto: ");
-		prod.setLargo(sc.nextFloat());
-		
-		System.out.println("Ud. desea enviar "+prod.getCantidad()+" "+prod.getNombre()+"(s)");
-		System.out.println("Peso: "+prod.getPeso());
-		System.out.println("Tipo: "+prod.getTipo());
-		System.out.println("Dimensiones:\nlargo: "+prod.getLargo()+" ancho: "+prod.getAncho()+" alto: "+prod.getAlto());
-		
-		
+		p.setNombre(obo = sc.nextLine());
+		return obo;
 	}
+	
+	public float pesoPro() {
+		float peso;
+		System.out.println("Por favor ingrese el peso del producto: ");
+		p.setPeso(peso = sc.nextFloat());
+		return peso;
+	}
+	
+	public int cantidadPro() {
+		int cantidad;
+		System.out.println("Por favor ingrese la cantidad de productos: \n");
+		p.setCantidad(cantidad = sc.nextInt());
+		return cantidad;
+	}
+	
+	public String tipoPro() {
+		String tipo;
+		System.out.println("Por favor ingrese el tipo de producto: ");
+		p.setTipo(tipo = sc.next());
+		return tipo;
+	}
+	
+	public float anchoPro() {
+		float ancho;
+		System.out.println("Por favor ingrese el ancho del producto: ");
+		p.setAncho(ancho = sc.nextFloat());
+		return ancho;
+	}
+	
+	public float largoPro() {
+		float largo;
+		System.out.println("Por favor ingrese el alto del producto: ");
+		p.setAlto(largo = sc.nextFloat());
+		return largo;
+	}
+	
+	public float altoPro() {
+		float alto;
+		System.out.println("Por favor ingrese el largo del producto: ");
+		p.setLargo(alto = sc.nextFloat());
+		return alto;
+	}
+	
+	public void datos() {
+		System.out.println("Ud. desea enviar "+p.getCantidad()+" "+p.getNombre()+"(s)");
+		System.out.println("Peso: "+p.getPeso());
+		System.out.println("Tipo: "+p.getTipo());
+		System.out.println("Dimensiones:\nlargo: "+p.getLargo()+" ancho: "+p.getAncho()+" alto: "+p.getAlto());	
+	}
+	
 }
--- project-git/src/test/java/ec/edu/epn/proyecto/git/IngresoDatosTest.java
+++ project-git/src/test/java/ec/edu/epn/proyecto/git/IngresoDatosTest.java
@@ -1,14 +1,78 @@
 package ec.edu.epn.proyecto.git;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class IngresoDatosTest {
 
+	 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void nombreProTest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("nombreProTest\n");
+        String actual = i.nombrePro();
+        System.out.println(actual);
+        String expected = "Perro";
+        assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void pesoProTest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("pesoProTest\n");
+        float actual = i.pesoPro();
+        System.out.println(actual);
+        float expected = 23;
+        assertEquals(expected, actual, 0);
+	}
+	@Test
+	public void cantidadProTest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("cantidadProTest\n");
+        int actual = i.cantidadPro();
+        System.out.println(actual);
+        int expected = 3;
+        assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void tipoProtest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("tipoProTest\n");
+        String actual = i.tipoPro();
+        System.out.println(actual);
+        String expected = "Animal";
+        assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void anchoProTest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("anchoProTest\n");
+        float actual = i.anchoPro();
+        System.out.println(actual);
+        float expected = 13;
+        assertEquals(expected, actual, 0);
+	}
+	
+	@Test
+	public void largoProTest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("largoProTest\n");
+        float actual = i.largoPro();
+        System.out.println(actual);
+        float expected = 15;
+        assertEquals(expected, actual, 0);
+	}
+	
+	@Test
+	public void altoProTest() {
+		IngresoDatos i = new IngresoDatos();
+        System.out.print("altoProTest\n");
+        float actual = i.altoPro();
+        System.out.println(actual);
+        float expected = 20;
+        assertEquals(expected, actual, 0);
+	}	
 
 }
